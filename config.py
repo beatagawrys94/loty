4bc1d91cf1da4c72e3b606e411067b978137973e
beatagawrys94 <46403981+beatagawrys94@users.noreply.github.com>
2025-06-28 11:29:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp-relay.sendinblue.com")
 SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
 SMTP_USER = os.getenv("SMTP_USER", "908317001@smtp-brevo.com")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "RM0ymJqpDF417nkg")
-EMAIL_TO = os.getenv("EMAIL_TO")
+EMAIL_TO = os.getenv("EMAIL_TO", "beatka1433@gmail.com")
 
 SKYSCANNER_URL = os.getenv("https://www.skyscanner.pl/transport/loty/wars/bkkt/251226/260114/?adultsv2=2&cabinclass=economy&childrenv2=&ref=home&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false")  # wkleimy stały link jako zmienną
 MAX_PRICE = int(os.getenv("MAX_PRICE", "3900"))
